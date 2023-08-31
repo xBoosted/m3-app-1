b2ce3ceee71fb37531295354710a452c696dfe8f
pelayojohnallen@gmail.com <pelayojohnallen@gmail.com>
2023-08-31 14:05:28 +0000

    Created Form1.Form2.RowTemplate1 and Form1.Form2; Edited settings and Form1

--- client_code/Form1/Form2/__init__.py
+++ client_code/Form1/Form2/__init__.py
@@ -1,16 +1,13 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Form2Template
 from anvil import *
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
-import anvil.server
 
-class Form1(Form1Template):
+class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    anvil.users.login_with_form()
-    self.customer_list_panel.items = anvil.server.call('get_customers')
